406cf7f139cf797c80982950276bc477d759fffb
erivas <erivas@tribalworldwide.gt>
2022-02-21 12:26:07 -0600

    Initial Commit

---
+++ viperExample/viperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  viperExampleApp.swift
+//  viperExample
+//
+//  Created by Tribal on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct viperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
